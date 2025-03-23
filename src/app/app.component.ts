c7fbc1bac46ef3958447e133a24b40f050223899
pabloMNXOnline <pablo@mnxonline.com>
2025-03-23 14:20:44 +0000

    Add: Controladores User y Project funcionales -- Error referencia de user_id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatGridListModule } from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, MatGridListModule,ProjectsComponent, MatCardModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mnxtrello-front';
+  title = 'MNXTrello';
 }
