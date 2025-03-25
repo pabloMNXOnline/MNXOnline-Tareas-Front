083398537ed0e81d6706940f4ace8a6c316c31a0
pabloMNXOnline <pablo@mnxonline.com>
2025-03-25 15:02:13 +0000

    Add:Datos mostrados en el front, insertar tarea y modificar tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {MatGridListModule } from '@angular/material/grid-list';
-import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, MatGridListModule,ProjectsComponent, MatCardModule,],
+  imports: [MatGridListModule,ProjectsComponent, MatCardModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
